b5dda7c9ca93571680c489567983ef3da60f4d34
Your Name~ <you@example.com>
2025-09-23 22:41:19 +0300

    update membership form to reflect user selection

--- script.js
+++ script.js
@@ -62,11 +62,41 @@ document.addEventListener('DOMContentLoaded', function() {
   // membership modal
   const membershipModal = document.getElementById('membershipModal');
   const applyMembershipBtn = document.querySelectorAll('.memb-btn');
+  // sections of the form
+  const personal_info = document.getElementById('personal-info');
+  const institute_info = document.getElementById('institutional-info');
+  const honorary_info = document.getElementById('honorary-info');
+  // function to show and hide necessary section
+  function toggleSections(type){
+    // hide all sections first
+    personal_info.style.display = 'none';
+    institute_info.style.display = 'none';
+    honorary_info.style.display = 'none';
+
+    // use switch case to toggle
+    switch(type){
+      case 'associate':
+        personal_info.style.display = 'grid';
+        break;
+      case 'institutional':
+        institute_info.style.display = 'grid';
+        break;
+      case 'honorary':
+        honorary_info.style.display = 'block';
+      break;
+    }
+  }
+
   // open membership modal
   applyMembershipBtn.forEach(btn=>{
-    btn.addEventListener('click',()=>{
+    btn.addEventListener('click',(e)=>{
       modalOverlay.style.display = 'block';
       membershipModal.style.display = 'block';
+      // get membership type
+      const membType = e.target.getAttribute('data-memb-type');
+
+      // show necessary section
+      toggleSections(membType);
     });
   });
   
@@ -104,16 +134,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  // // user submissions
-  // const submitBtns = document.querySelectorAll('.submitBtn');
-  // submitBtns.forEach(btn => {
-  //   btn.addEventListener('click', (e) => {
-  //     e.preventDefault();
-  //     const form = btn.closest('form'); // find the parent form
-  //     alert ("Form successfully submitted:", form);
-  //     form.reset();
-  // });
-  // });
 
 
   // validate user input and reset form
@@ -122,27 +142,44 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit',(e)=>{
       e.preventDefault();
 
-      // validate the emergency contacts are unique
-      const tel1 = document.getElementById('tel1').value.trim();
-      const tel2 = document.getElementById('tel2').value.trim();
-      const existingMessage = document.getElementById('message');
-      // remove existing message
-      if(existingMessage){
-        existingMessage.remove()
-      }
-      // validate user input
-      if(tel1 === tel2){
-        const message = document.createElement('p');
-        message.classList.add('message');        
-        message.innerHTML='please provide two unique contact persons';
-        //append to appear after name2 input
-        const tel2Input = document.getElementById('tel2');
-        tel2Input.parentNode.appendChild(message);
-        return;
-      }
-      forms.reset();      
+      // // find form that trigerred event
+      // const targetForm =e.target.closest('form');
+      if (validateForm(e.target)){
+        alert('Form submitted successfully');
+        e.target.reset(); 
+      }     
     });
   });
+
+  // validate the emergency contacts are unique
+  function validateForm(){
+    const tel1 = document.getElementById('tel1').value.trim();
+    const tel2 = document.getElementById('tel2').value.trim();
+    const existingMessage = document.querySelector('.message');
+    // remove existing message
+    if(existingMessage){
+      existingMessage.remove();
+    }
+
+    // validate user input
+    const message = document.createElement('p');
+    message.classList.add('message');
+    if(tel1 === tel2){        
+      message.innerHTML='please provide two unique contact persons';
+      //append to appear after name2 input
+      const tel2Input = document.getElementById('tel2');
+      tel2Input.parentNode.appendChild(message);
+      return false;
+    }else{
+      message.innerHTML ='unique contacts provided';
+      message.style.color = 'green';
+      const tel2Input = document.getElementById('tel2');
+      tel2Input.parentNode.appendChild(message);
+      return true;
+    }
+
+           
+  }
 
 
 });
